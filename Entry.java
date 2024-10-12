a34e11b965afce90f750aa1fd7997df962ae1df4
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-10-12 11:26:11 -0300

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -1,6 +1,8 @@
-public class Entry<K, V> {
+// Classe Entry
+public class Entry <K, V>{
     K chave;
     V valor;
+
     public Entry(K chave, V valor){
         this.chave = chave;
         this.valor = valor;
